ee6bbe7e01646e7e624cef8ef38adeb6a9f269a3
gdouselwc <15361093829@163.com>
2018-07-25 23:20:10 +0800

    springboot start

---
+++ src/main/java/com/java/SpringbootworkApplication.java
@@ -0,0 +1,12 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootworkApplication.class, args);
+	}
+}
